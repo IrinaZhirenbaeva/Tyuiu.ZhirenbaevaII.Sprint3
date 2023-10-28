e41958a7c4267590472fc30f712d5cf54d1dd9ae
KK <KK@DESKTOP-TIELPRS>
2023-10-29 03:18:56 +0500

    Task6

---
+++ Tyuiu.ZhirenbaevaII.Sprint3.Task6.V7.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZhirenbaevaII.Sprint3.Task6.V7.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZhirenbaevaII.Sprint3.Task6.V7.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ca90e1b9-d2d9-4df9-98b9-b755e7a52568")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ZhirenbaevaII.Sprint3.Task6.V7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ZhirenbaevaII.Sprint3.Task6.V7.Lib;
+
+namespace Tyuiu.ZhirenbaevaII.Sprint3.Task6.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Жиренбаева И.И| ИСТНб-23-1";
+
+            Console.WriteLine("**");
+            Console.WriteLine(" Спринт #3                                                               ");
+            Console.WriteLine(" Тема: Алгоритмы циклической структуры                                      ");
+            Console.WriteLine(" Задание #6                                                            ");
+            Console.WriteLine(" Вариант #7                                                            ");
+            Console.WriteLine(" Выполнила: Жиренбаева Ирина Ильгизовна | ИСТНб-23-1                     ");
+            Console.WriteLine("**");
+            Console.WriteLine("* УСЛОВИЕ:                                                               ");
+            Console.WriteLine(" Напишите программу, которая ищет среди целых чисел,                         ");
+            Console.WriteLine(" принадлежащих числовому отрезку [17, 26] сумму всех делителей                                                                        ");
+            Console.WriteLine("**");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       ");
+            Console.WriteLine("**");
+
+            int startValue = 17;
+            int stopValue = 26;
+
+            Console.WriteLine("Начало отрезка =  " + startValue);
+            Console.WriteLine("Конец отрезка =  " + stopValue);
+          
+            Console.WriteLine("**");
+            Console.WriteLine(" РЕЗУЛЬТАТ:                                                              ");
+            Console.WriteLine("**");
+
+            Console.WriteLine("Сумма делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
