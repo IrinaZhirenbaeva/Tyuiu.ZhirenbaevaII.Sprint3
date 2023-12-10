2fc7e8aeb3efd835b21032697b9422e3d4dc7522
KK <KK@DESKTOP-TIELPRS>
2023-12-10 22:28:23 +0500

    Task3

---
+++ Tyuiu.ZhirenbaevaII.Sprint3.Task3.V16.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZhirenbaevaII.Sprint3.Task3.V16.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZhirenbaevaII.Sprint3.Task3.V16.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2aab900c-2b60-477f-ba50-23f0d089e67c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ZhirenbaevaII.Sprint3.Task3.V16/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ZhirenbaevaII.Sprint3.Task3.V16.Lib;
+
+namespace Tyuiu.ZhirenbaevaII.Sprint3.Task3.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Жиренбаева И.И| ИСТНб-23-1";
+
+            Console.WriteLine("**");
+            Console.WriteLine(" Спринт #3                                                               ");
+            Console.WriteLine(" Тема: Алгоритмы циклической структуры                                       ");
+            Console.WriteLine(" Задание #3                                                             ");
+            Console.WriteLine(" Вариант #16                                                           ");
+            Console.WriteLine(" Выполнила: Жиренбаева Ирина Ильгизовна | ИСТНб-23-1                    ");
+            Console.WriteLine("**");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Используя цикл foreach подсчитать количество букв “c” в строке ice      *");
+            Console.WriteLine("* nice ice creamcc                                                        *");
+            Console.WriteLine("**");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        ");
+            Console.WriteLine("**");
+            string value = "ice nice ice creamcc";
+            char chr = 'c';
+
+            Console.WriteLine(" Исходная строка = " + value);
+            Console.WriteLine(" Искомый символ = " + chr);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(" Количество символов = " + ds.GetCharCount(value, chr));
+
+            Console.ReadKey();
+        }
+    }
+    
+}
