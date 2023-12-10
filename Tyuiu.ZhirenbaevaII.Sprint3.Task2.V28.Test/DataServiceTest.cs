7d60c24b5c5866f8320dd9c999459605db95d8d7
KK <KK@DESKTOP-TIELPRS>
2023-12-10 22:21:41 +0500

    Task2

---
+++ Tyuiu.ZhirenbaevaII.Sprint3.Task2.V28.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ZhirenbaevaII.Sprint3.Task2.V28.Lib;
+
+namespace Tyuiu.ZhirenbaevaII.Sprint3.Task2.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 9;
+            double res = ds.GetMultiplySeries(startValue, stopValue);
+            double wait = -15.642;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ZhirenbaevaII.Sprint3.Task2.V28.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZhirenbaevaII.Sprint3.Task2.V28.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZhirenbaevaII.Sprint3.Task2.V28.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0e7a14bd-ea45-40de-8535-e342d4691b3b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
